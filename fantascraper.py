633f6c550f370357e111f4ae2cfa2b400e2b8953
Sam Oehlert <sam.oehlert@gmail.com>
2018-09-04 23:06:42 -0500

    used black to update formatting and fix some pep8 issues

--- fantascraper.py
+++ fantascraper.py
@@ -3,7 +3,6 @@
 import csv
 import sys
 import argparse
-import requests
 
 from mechanize import Browser
 from configparser import ConfigParser
@@ -11,62 +10,82 @@ from BeautifulSoup import BeautifulSoup
 
 # Weird names in EPL from other alphabets
 reload(sys)
-sys.setdefaultencoding('utf-8')
+sys.setdefaultencoding("utf-8")
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--config", help="Specify config file location", default="fantascraper.conf", dest='conf_file')
-parser.add_argument("-t", "--team", help="Specify team file location", default="team.csv", dest='team_file')
-parser.add_argument("--version", help="Print version", action="version", version="%(prog)s 0.1.0-alpha")
+parser.add_argument(
+    "-c",
+    "--config",
+    help="Specify config file location",
+    default="fantascraper.conf",
+    dest="conf_file",
+)
+parser.add_argument(
+    "-t",
+    "--team",
+    help="Specify team file location",
+    default="team.csv",
+    dest="team_file",
+)
+parser.add_argument(
+    "--version", help="Print version", action="version", version="%(prog)s 0.1.0-alpha"
+)
 args, remaining_argv = parser.parse_known_args()
 
 team_file = args.team_file
 conf_file = args.conf_file
 conf = ConfigParser()
 conf.read([conf_file])
-league_id = conf.get('config', 'league_id')
-team_id = conf.get('config', 'team_id')
+league_id = conf.get("config", "league_id")
+team_id = conf.get("config", "team_id")
 
-roster_table_outfield = 'rosterTable_5010'
-roster_table_keepers = 'rosterTable_5020'
+roster_table_outfield = "rosterTable_5010"
+roster_table_keepers = "rosterTable_5020"
 mech = Browser()
-url = 'http://www.fantrax.com/newui/fantasy/teamRoster.go?teamId={}&leagueId={}&isSubmit=y'.format(team_id,league_id)
+url = "http://www.fantrax.com/newui/fantasy/teamRoster.go?teamId={}&leagueId={}&isSubmit=y".format(
+    team_id, league_id
+)
 page = mech.open(url)
 html = page.read()
 soup = BeautifulSoup(html)
 
 players = []
 
+
 def rename_player(first, last):
     player = first, last
     players.append(player)
     return players
 
+
 def find_players(soup, roster_table):
-    player_table = soup.find('table', {'id':roster_table})
-    for row in player_table.findAll('tr'):
-        names = row.findAll('a', {'class': 'hand '})
+    player_table = soup.find("table", {"id": roster_table})
+    for row in player_table.findAll("tr"):
+        names = row.findAll("a", {"class": "hand "})
         for name in names:
-            if name.find('img'):
+            if name.find("img"):
                 continue
             else:
-                if ',' not in name.string:
-                    if ' ' not in name.string:
+                if "," not in name.string:
+                    if " " not in name.string:
                         players.append(str(name.string))
                     else:
-                        first, last = name.string.split(' ')
+                        first, last = name.string.split(" ")
                         rename_player(str(first), str(last))
                 else:
-                    last, first = name.string.split(', ')
+                    last, first = name.string.split(", ")
                     rename_player(str(first), str(last))
     return players
 
+
 def write_players(players, team_file):
-    with open(team_file, 'wb') as team:
-        f = csv.writer(team,delimiter=' ',quoting = csv.QUOTE_NONE,escapechar='\\')
+    with open(team_file, "wb") as team:
+        f = csv.writer(team, delimiter=" ", quoting=csv.QUOTE_NONE, escapechar="\\")
         for player in players:
             f.writerow(player)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     find_players(soup, roster_table_outfield)
     find_players(soup, roster_table_keepers)
     write_players(players, team_file)
--- physioroom.py
+++ physioroom.py
@@ -10,28 +10,43 @@ from BeautifulSoup import BeautifulSoup
 
 # Weird names in EPL from other alphabets
 reload(sys)
-sys.setdefaultencoding('utf-8')
+sys.setdefaultencoding("utf-8")
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--config", help="Specify config file location", default="physioroom.conf", dest='conf_file')
-parser.add_argument("-t", "--team", help="Specify team file location", default="team.csv", dest='team_file')
-parser.add_argument("--version", help="Print version", action="version", version="%(prog)s 0.5.0-beta")
+parser.add_argument(
+    "-c",
+    "--config",
+    help="Specify config file location",
+    default="physioroom.conf",
+    dest="conf_file",
+)
+parser.add_argument(
+    "-t",
+    "--team",
+    help="Specify team file location",
+    default="team.csv",
+    dest="team_file",
+)
+parser.add_argument(
+    "--version", help="Print version", action="version", version="%(prog)s 0.5.0-beta"
+)
 args, remaining_argv = parser.parse_known_args()
 
 team_file = args.team_file
 conf_file = args.conf_file
 conf = ConfigParser()
 conf.read([conf_file])
-slack_channel = conf.get('slack', 'channel_id')
-slack_token = conf.get('slack', 'token')
+slack_channel = conf.get("slack", "channel_id")
+slack_token = conf.get("slack", "token")
 
 slack_client = SlackClient(slack_token)
 
 mech = Browser()
-url = 'http://premierinjuries.com/injury-table.php'
+url = "http://premierinjuries.com/injury-table.php"
 page = mech.open(url)
 html = page.read()
 soup = BeautifulSoup(html)
+
 
 def send_message(channel_id, message):
     """ Post injured list to slack """
@@ -39,34 +54,47 @@ def send_message(channel_id, message):
         "chat.postMessage",
         channel=channel_id,
         text=message,
-        username='Injurybot',
-        icon_emoji=':hospital:'
+        username="Injurybot",
+        icon_emoji=":hospital:",
     )
+
 
 def get_team(team_file):
     """ Get list of players on our team """
     players = []
     with open(team_file) as team:
         for player in team:
-            player = player.replace('\\','')
+            player = player.replace("\\", "")
             players.append(player.rstrip())
     return players
+
 
 def get_epl_injured():
     """ Get a list of all the injuries in the EPL """
     epl_injured = []
 
-    table = soup.find('table', {'class':'injury-table'})
-    for row in table.findAll('tr'):
-        players = [ row.find('td') ]
+    table = soup.find("table", {"class": "injury-table"})
+    for row in table.findAll("tr"):
+        players = [row.find("td")]
         for player in players:
-            if player == None:
+            if player is None:
                 break
-            elif not player.find('div'):
+            elif not player.find("div"):
                 break
             else:
-                epl_injured.append(str(''.join(text for text in player.findAll(text=True) if text.parent.name != 'div').rstrip().lower()))
+                epl_injured.append(
+                    str(
+                        "".join(
+                            text
+                            for text in player.findAll(text=True)
+                            if text.parent.name != "div"
+                        )
+                        .rstrip()
+                        .lower()
+                    )
+                )
     return epl_injured
+
 
 def check_players(players, epl_injured):
     """ Check if anyone on our team is injured """
@@ -76,7 +104,8 @@ def check_players(players, epl_injured):
             injured.append(player)
     return injured
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     team = get_team(team_file)
     epl = get_epl_injured()
     injured = check_players(team, epl)
