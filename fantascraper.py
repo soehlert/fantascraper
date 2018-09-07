b89c3c2973851a4fe617342bc435c2030baeff8d
Sam Oehlert <sam.oehlert@gmail.com>
2018-09-07 01:38:05 -0500

    update fantascraper to fix merge issues

--- fantascraper.py
+++ fantascraper.py
@@ -10,28 +10,41 @@ from BeautifulSoup import BeautifulSoup
 
 # Weird names in EPL from other alphabets
 reload(sys)
-sys.setdefaultencoding('utf-8')
+sys.setdefaultencoding("utf-8")
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--config", help="Specify config file location",
-                    default="fantascraper.conf", dest='conf_file')
-parser.add_argument("-t", "--team", help="Specify team file location",
-                    default="team.csv", dest='team_file')
-parser.add_argument("--version", help="Print version", action="version",
-                    version="%(prog)s 0.1.0-alpha")
+parser.add_argument(
+    "-c",
+    "--config",
+    help="Specify config file location",
+    default="fantascraper.conf",
+    dest="conf_file",
+)
+parser.add_argument(
+    "-t",
+    "--team",
+    help="Specify team file location",
+    default="team.csv",
+    dest="team_file",
+)
+parser.add_argument(
+    "--version", help="Print version", action="version", version="%(prog)s 0.1.0-alpha"
+)
 args, remaining_argv = parser.parse_known_args()
 
 team_file = args.team_file
 conf_file = args.conf_file
 conf = ConfigParser()
 conf.read([conf_file])
-league_id = conf.get('config', 'league_id')
-team_id = conf.get('config', 'team_id')
+league_id = conf.get("config", "league_id")
+team_id = conf.get("config", "team_id")
 
-roster_table_outfield = 'rosterTable_5010'
-roster_table_keepers = 'rosterTable_5020'
+roster_table_outfield = "rosterTable_5010"
+roster_table_keepers = "rosterTable_5020"
 mech = Browser()
-team_url = 'http://www.fantrax.com/newui/fantasy/teamRoster.go?teamId={}&leagueId={}&isSubmit=y'.format(team_id, league_id)
+team_url = "http://www.fantrax.com/newui/fantasy/teamRoster.go?teamId={}&leagueId={}&isSubmit=y".format(
+    team_id, league_id
+)
 page = mech.open(team_url)
 html = page.read()
 soup = BeautifulSoup(html)
@@ -46,34 +59,33 @@ def rename_player(first, last):
 
 
 def find_players(soup, roster_table):
-    player_table = soup.find('table', {'id': roster_table})
-    for row in player_table.findAll('tr'):
-        names = row.findAll('a', {'class': 'hand '})
+    player_table = soup.find("table", {"id": roster_table})
+    for row in player_table.findAll("tr"):
+        names = row.findAll("a", {"class": "hand "})
         for name in names:
-            if name.find('img'):
+            if name.find("img"):
                 continue
             else:
-                if ',' not in name.string:
-                    if ' ' not in name.string:
+                if "," not in name.string:
+                    if " " not in name.string:
                         players.append(str(name.string))
                     else:
-                        first, last = name.string.split(' ')
+                        first, last = name.string.split(" ")
                         rename_player(str(first), str(last))
                 else:
-                    last, first = name.string.split(', ')
+                    last, first = name.string.split(", ")
                     rename_player(str(first), str(last))
     return players
 
 
 def write_players(players, team_file):
-    with open(team_file, 'wb') as team:
-        csv_f = csv.writer(team, delimiter=' ',
-                           quoting=csv.QUOTE_NONE, escapechar='\\')
+    with open(team_file, "wb") as team:
+        csv_f = csv.writer(team, delimiter=" ", quoting=csv.QUOTE_NONE, escapechar="\\")
         for player in players:
             csv_f.writerow(player)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     find_players(soup, roster_table_outfield)
     find_players(soup, roster_table_keepers)
     write_players(players, team_file)
